c5d683129871f862fb8136b2ec53301b72425ebc
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-05-24 12:26:56 +0400

    Update ATouchpad control relay with TTP223.py

--- ATouchpad control relay with TTP223.py
+++ ATouchpad control relay with TTP223.py
@@ -1,6 +1,6 @@
 from machine import Pin #importing Pin class from Machine module
 import time#importing time module
-Touch_pad = machine.Pin(17,Pin.IN,Pin.PULL_UP)#creating an object(Touch_pad) and assigning the Pin 17 to as an OUTPUT.
+Touch_pad = machine.Pin(17,Pin.IN,Pin.PULL_UP)#creating an object(Touch_pad) and assigning the Pin 17 to as an INPUT.
 led = Pin(15,Pin.OUT)#creating an object(led) and assigning the Pin 15 to as an OUTPUT.
 Relay=Pin(28,Pin.OUT)#creating an object(Relay) and assigning the Pin 28 to as an OUTPUT.
 while True:
